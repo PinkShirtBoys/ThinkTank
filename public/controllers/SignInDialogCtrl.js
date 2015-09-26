a5a62a10e888c8678188b8c8060dc99c08cc07cc
pranavpunjabi@gmail.com <pranavpunjabi@gmail.com>
2015-09-26 16:21:53 -0400

    SignIn implemented

--- public/controllers/SignInDialogCtrl.js
+++ public/controllers/SignInDialogCtrl.js
@@ -8,6 +8,14 @@ myApp.controller('SignInDialogCtrl',['ngDialog',function(ngDialog){
 	this.submit = function(){
 		console.log("username : " + this.model.username);
 		console.log("password : " + this.model.password);
+
+		Parse.User.logIn(this.model.username, this.model.password).then(function(user){
+			console.log("User logged in");
+		}, function(error) {
+			console.log("Error logging in");
+
+		});
+
 		ngDialog.closeAll();
 	}	
 }]);
