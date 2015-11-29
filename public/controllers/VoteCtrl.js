685a52e856dbc3afc59ad07c1e4a7acee32fd59b
Rohan Barman <Rohan1Barman@gmail.com>
2015-11-29 18:20:22 -0500

    MVP - bootstrap vote view * Once the debate is over (maximum for and against arguments), vote.html is ng-included * If the current user is logged in then he can select radio buttons to vote for a side

---
+++ public/controllers/VoteCtrl.js
@@ -0,0 +1,10 @@
+var myApp = angular.module('myApp');
+myApp.controller('VoteCtrl',[function(){
+
+	// TODO vote with DebateService
+	// keep track of timelimit and a person can only vote once
+	this.vote = function() {
+		console.log("TODO : Will add vote functionality");
+	}
+
+}]);
