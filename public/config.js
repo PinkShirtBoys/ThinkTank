b0d4fe523c589492eff2c411615c64818aba6b0d
Rohan Barman <Rohan1Barman@gmail.com>
2015-12-01 18:10:48 -0500

    remove public/config.js from gitignore * There is no value in hiding this config.js because anyone can easily access config.js by inspecting the web app * Need to create Parse security rules to preven unwanted api manipulation * Also removing this from .gitignore makes deployment to heroku or any push to deploy service very easy.

---
+++ public/config.js
@@ -0,0 +1,12 @@
+// config.js will hold all password and authorization info
+
+var ParseCredentials = {
+	applicationId : 'sb60iaVk7DTr9g3JZ0zfpqLIlK8lM2s6WVzbPaED',
+	javascriptKey : 'lWOfrM6Son29gBIZ84kaPwvNmXJY1klvMc4oFc3J',
+	masterKey :"mL3dI5WUoG1ItABWncosWNsXzEOZZG5yqcSmecv7"
+}
+
+var PubNubCredentials = {
+	publish_key:'pub-c-3586a89b-d26d-4220-8f4d-f38169d330d9',
+	subscribe_key:'sub-c-43c3ad96-97ea-11e5-9a49-02ee2ddab7fe'
+}
